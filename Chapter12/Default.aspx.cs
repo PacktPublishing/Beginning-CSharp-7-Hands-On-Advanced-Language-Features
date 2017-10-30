21036244c2ebf44a3d01261d55cd4dc6a63fb46e
packtBhagyashree <bhagyashreer@packtpub.com>
2017-10-30 10:34:15 +0530

    Added code files

---
+++ Chapter12/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿//1. using is a keyword, it's blue
+//2. System is a name space that stores already created code
+//3. using System brings in existing code
+using System;
+//4. class is a required container for creating our own code samples
+public partial class _Default : System.Web.UI.Page
+{
+    //5. Code below is where we place our own code
+    //6. Page_Load is code that runs when a page loads from a server
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        double xOut, yOut;//7. Purpose of these variable is to try to set them using try parse
+        //8. Line below combines the ternary operator with TryParse
+        //9. If conversion works, xOut has the intended value and it's then saved to x
+        //10. If conversion fails, xOut has the value 0, but I've chosen to just save 0 to x
+        double x = double.TryParse(TextBox1.Text, out xOut) ? xOut :0;
+
+        //11. Line below combines the ternary operator with TryParse
+        //12. If conversion works, yOut has the intended value and it's then saved to y
+        //13. If conversion fails, yOut has the value 0, but I've chosen to just save 0 to y explicitly
+        double y = double.TryParse(TextBox2.Text, out yOut) ? yOut : 0;
+
+        //14. x+y are added, and the result is saved into the string for printing
+        Label1.Text = $"Your sum is {x + y}";
+    }
+}
+
---
+++ Chapter15/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>    
+    <form id="form1" runat="server">
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+        <asp:Button ID="Button1" runat="server" Text="Find Sum" OnClick="Button1_Click" /><br />
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+    </form>
+</body>
+</html>
+
